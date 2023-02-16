1732c7b19b3c3d0b1e099aebc52bd2c8c40686ae
sabbir hossain <mdsabbir477470@gmail.com>
2023-02-17 03:08:12 +0600

    homeTop done

--- src/Pages/Cto-services/Home/Home.js
+++ src/Pages/Cto-services/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import HomeTop from '../HomeTop/HomeTop';
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <HomeTop></HomeTop>
+            <About></About>
+            <Contact></Contact>
         </div>
     );
 };
---
+++ src/Pages/Cto-services/HomeTop/HomeTop.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import banner from '../../../Images/banner.png';
+import './HomeTop.css';
+
+const HomeTop = () => {
+    return (
+        
+        <div className='home-Banner p-1' id='home'>
+             {/* <HomeBanner section start /> */}
+            <div className="position-fixed fixed-bottom text-end me-4 mb-4">
+                  <a href="#home">
+                      <img className='img-fluid go-to-top-icon' src='https://icon-library.com/images/back-to-top-icon-png/back-to-top-icon-png-7.jpg' alt="back-to-top-icon" />
+                  </a>
+              </div>
+            <div className='position-relative w-100 h-100 home-top'>
+
+            </div>
+            <Container className='mt-5 my-5'>
+                <Row>
+                    <Col md='6'>
+                        <h1 className='fs-1 font-bold cto-title'>CTO (Chief Technology Officer)</h1>
+                        <br />
+                        <p className='fs-5'>The CTO has overall responsibility for managing the physical and personnel technology infrastructure including technology deployment, network and system management, integration testing, and developing technical operations personnel. CTOs also manage client relations to ensure that service objective expectations are developed and managed in the operations areas.Generally, a chief technology officer has a degree in computer science or engineering, has a strong knowledge of software architecture, design, and programming. Since a CTO should solve engineering problems, a solid development background is required. Business awareness.</p></Col>
+                    <Col md='6'><img className='banner rounded-md' src={banner} alt="banner" data-aos="fade-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000" /></Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default HomeTop;
